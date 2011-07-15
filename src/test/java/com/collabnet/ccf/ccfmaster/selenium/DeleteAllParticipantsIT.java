37df4e886cf4fdc0c0290ef4c93646bb70cd570e
ccmtaylor <ccmtaylor@collab.net>
2011-07-15 14:22:31 +0000

    get selenium baseUrl form ccf.baseUrl system property and use http://localhost:8080/ as default value. Path is ignored in selenium, only host/port is relevant, so this should work.
    git-svn-id: https://ctf.open.collab.net/svn/repos/ccf/branches/maven-selenium-cobertura-spike/CCFMaster@4650 6771e1e2-0f46-0410-b5d7-d846ff89f743

--- src/test/java/com/collabnet/ccf/ccfmaster/selenium/DeleteAllParticipantsIT.java
+++ src/test/java/com/collabnet/ccf/ccfmaster/selenium/DeleteAllParticipantsIT.java
@@ -1,22 +1,38 @@
 package com.collabnet.ccf.ccfmaster.selenium;
 
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverBackedSelenium;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.thoughtworks.selenium.SeleneseTestBase;
 
 public class DeleteAllParticipantsIT extends SeleneseTestBase {
 //	private Selenium selenium;
+	private static Logger log = LoggerFactory.getLogger(DeleteAllParticipantsIT.class);
 
-	@Before
+	@BeforeClass
 	public void setUp() throws Exception {
 		WebDriver driver = new FirefoxDriver();
-		String baseUrl = "http://localhost:8080/";
+		String baseUrl = System.getProperty("ccf.baseUrl","http://localhost:8080/");
+		log.info("Using {} as baseUrl.", baseUrl);
 		selenium = new WebDriverBackedSelenium(driver, baseUrl);
+	}
+	
+	@Before
+	public void login() {
+		selenium.open("/CCFMaster/login");
+		selenium.click("j_username");
+		selenium.type("j_username", "admin");
+		selenium.type("j_password", "admin");
+		selenium.click("proceed");
+		selenium.waitForPageToLoad("30000");
 	}
 
 	@Test
@@ -30,6 +46,11 @@ public class DeleteAllParticipantsIT extends SeleneseTestBase {
 	}
 
 	@After
+	public void logout() {
+		selenium.open("/CCFMaster/resources/j_spring_security_logout");
+	}
+	
+	@AfterClass
 	public void tearDown() throws Exception {
 		selenium.stop();
 	}
