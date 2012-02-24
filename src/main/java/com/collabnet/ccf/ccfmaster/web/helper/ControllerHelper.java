dc8b3e52c53f042813bc6c8ee690721f6f371687
balajikandan <kbalaji@collab.net>
2012-02-24 09:50:36 +0000

    [artf6897]: Expose a REST endpoint to update CCF Cores (of an associated landscape)
    Changes done:
    Missed one review comment(a landscape does not already exist) from Johannes, no it is fixed.
    
    ControllerHelper.java - findLandscape method now returns null if there is no landscape available. And also landscapeDirName currently returns landscape dir only if landscape available else it returns its parent dir name.
    
    git-svn-id: https://ctf.open.collab.net/svn/repos/ccf/trunk/CCFMaster@5297 6771e1e2-0f46-0410-b5d7-d846ff89f743

--- src/main/java/com/collabnet/ccf/ccfmaster/web/helper/ControllerHelper.java
+++ src/main/java/com/collabnet/ccf/ccfmaster/web/helper/ControllerHelper.java
@@ -8,12 +8,15 @@ public class ControllerHelper {
 	
 	public static Landscape findLandscape(){
 		List<Landscape> landscapeList=Landscape.findAllLandscapes();		
-		return landscapeList.get(0);
+		return landscapeList.isEmpty()? null: landscapeList.get(0);
 	}
 
-	
 	public static String landscapeDirName(String ccfHome){
-		return String.format("%s/landscape%d",ccfHome,findLandscape().getId());
+		String dirName = ccfHome;
+		if(findLandscape()!= null){
+			dirName = String.format("%s/landscape%d",ccfHome,findLandscape().getId());
+		}
+		return dirName;
 	}
 	
 }
