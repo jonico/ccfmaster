8692c3e025d4874561254ee93f18cc0b2ad1a49e
skumarvs <selvakumar@collab.net>
2012-04-19 04:54:43 +0000

    [artf7204] Spike: Issue warning message for unsaved form data on switching tabs
    ccf-functions.js - Added reusable jquery script
    Updated the form based jsp's to have the reference of the newly added script.
    
    Usage - In jsp add reference to jquery-1.6.2.min.js and 
    ccf-functions.js. Add class="submit" to the submit buttons.
    
    git-svn-id: https://ctf.open.collab.net/svn/repos/ccf/trunk/CCFMaster@5337 6771e1e2-0f46-0410-b5d7-d846ff89f743

--- src/main/webapp/js/ccf-functions.js
+++ src/main/webapp/js/ccf-functions.js
@@ -2,7 +2,12 @@
  * Javascript utilities functions that are reused
  */
 
+/*=========================================================================================*/
 
+/*
+ * Java script to submit the form when enter key is pressed.
+ * 
+ */
 function submitenter(myfield,e)
 		{
 		var keycode;
@@ -18,3 +23,32 @@ function submitenter(myfield,e)
 		else
 		   return true;
 		}
+
+/*=========================================================================================*/
+
+/*
+ * Jquery script to alert the user on unsaved form data on switching tabs,user closes browser window, or 
+ * navigation link or submits the form.
+ * add class="submit" to the form submit button to have the jquery work properly
+ * 
+ */
+
+$(document).ready(function() {
+    // Set the unload message whenever any input element get changed in the form.
+    $(':input').change(function() {
+        setConfirmUnload(true);
+    });
+
+    // Turn off the unload message whenever a form get submitted properly.
+    $('.submit').click(function() {
+        setConfirmUnload(false);
+    });
+});
+
+function setConfirmUnload(on) {
+    var message = "You have unsaved changes in the form.";
+    window.onbeforeunload = (on) ? function() { return message; } : null;
+}
+
+
+/*=========================================================================================*/
