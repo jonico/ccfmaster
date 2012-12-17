33eefb98d2da0e17a78eaf54ca5667e2e1eb58b8
balajikandan <kbalaji@collab.net>
2012-12-17 10:06:12 +0000

    [artf7599]: Follow PBI: Making RMD(Participant repository selection) implementation generic
    Changes done:
    
    Fixing creation of RMD for given direction
    
    git-svn-id: https://ctf.open.collab.net/svn/repos/ccf/branches/CCF%202.2@5529 6771e1e2-0f46-0410-b5d7-d846ff89f743

--- src/main/java/com/collabnet/ccf/ccfmaster/gp/validator/custom/DefaultCustomRMDParticipant.java
+++ src/main/java/com/collabnet/ccf/ccfmaster/gp/validator/custom/DefaultCustomRMDParticipant.java
@@ -2,33 +2,27 @@ package com.collabnet.ccf.ccfmaster.gp.validator.custom;
 
 import java.util.List;
 
+import org.apache.commons.lang.StringUtils;
+
 import com.collabnet.ccf.ccfmaster.gp.web.model.RMDModel;
 import com.collabnet.ccf.ccfmaster.gp.web.rmd.ICustomizeRMDParticipant;
 import com.collabnet.ccf.ccfmaster.server.domain.CCFCoreProperty;
 import com.collabnet.ccf.ccfmaster.server.domain.ConflictResolutionPolicy;
 
-public class JIRACustomRMDParticipant implements ICustomizeRMDParticipant{
+public class DefaultCustomRMDParticipant implements ICustomizeRMDParticipant{
+	
+	public static final String HYPHEN = "-";
 
 	@Override
 	public String getParticipantRepositoryId(RMDModel model) {
 		StringBuilder builder = new StringBuilder();
+		String repoId = null;
 		List<CCFCoreProperty> configProperties = model.getParticipantSelectorFieldList();
 		for (CCFCoreProperty property : configProperties) { 
-			String name = property.getName();
-			if(name.equalsIgnoreCase("jiraCollection")){
-				builder.append(property.getValue());
-				continue;
-			}
-			if(name.equalsIgnoreCase("jiraProjectList")){
-				builder.append("-").append(property.getValue());
-				continue;
-			}
-			if(name.equalsIgnoreCase("jiraWorkItemType")){
-				builder.append("-").append(property.getValue());
-				continue;
-			}
+			builder.append(property.getValue()).append(HYPHEN);
 		}
-		return builder.toString();
+		repoId = StringUtils.chomp(builder.toString(), HYPHEN);
+		return repoId;
 	}
 
 	@Override
--- src/main/java/com/collabnet/ccf/ccfmaster/gp/validator/custom/TFSCustomRMDParticipant.java
+++ src/main/java/com/collabnet/ccf/ccfmaster/gp/validator/custom/TFSCustomRMDParticipant.java
@@ -3,11 +3,9 @@ package com.collabnet.ccf.ccfmaster.gp.validator.custom;
 import java.util.List;
 
 import com.collabnet.ccf.ccfmaster.gp.web.model.RMDModel;
-import com.collabnet.ccf.ccfmaster.gp.web.rmd.ICustomizeRMDParticipant;
 import com.collabnet.ccf.ccfmaster.server.domain.CCFCoreProperty;
-import com.collabnet.ccf.ccfmaster.server.domain.ConflictResolutionPolicy;
 
-public class TFSCustomRMDParticipant implements ICustomizeRMDParticipant{
+public class TFSCustomRMDParticipant extends DefaultCustomRMDParticipant{
 
 	@Override
 	public String getParticipantRepositoryId(RMDModel model) {
@@ -29,16 +27,6 @@ public class TFSCustomRMDParticipant implements ICustomizeRMDParticipant{
 			}
 		}
 		return builder.toString();
-	}
-
-	@Override
-	public String[] getCustomConflictResolutionPolicy() {
-		ConflictResolutionPolicy[]  conflictValues = ConflictResolutionPolicy.values();
-		String[] conflictPolicyArray = new String[conflictValues.length];
-		for(int i=0;i<conflictValues.length;i++){
-			conflictPolicyArray[i]= conflictValues[i].toString();
-		}
-		return conflictPolicyArray;
 	}
 
 }
