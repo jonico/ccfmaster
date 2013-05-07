3cb697ebb97134972cf3f7769f9f8d93a6d5c84c
balajikandan <kbalaji@collab.net>
2013-05-07 12:51:47 +0000

    [artf7659]: Find a way to develop and package for thrid party plugin for CCFMaster
    Changes done:
    
    Fixing the Junit test case
    
    git-svn-id: https://ctf.open.collab.net/svn/repos/ccf/branches/CCF%202.2@5634 6771e1e2-0f46-0410-b5d7-d846ff89f743

--- src/test/java/com/collabnet/ccf/ccfmaster/server/core/SingleLandscapeCCFCoreInteractionStrategyTest.java
+++ src/test/java/com/collabnet/ccf/ccfmaster/server/core/SingleLandscapeCCFCoreInteractionStrategyTest.java
@@ -122,7 +122,7 @@ public class SingleLandscapeCCFCoreInteractionStrategyTest extends
 		}
 		
 		// TODO Find a better test for field mapping landscape template creation 
-		assertEquals(92, FieldMappingRule.findAllFieldMappingRules().size());
+		assertEquals(90, FieldMappingRule.findAllFieldMappingRules().size());
 	}
 
 	@Test
